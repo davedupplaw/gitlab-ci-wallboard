c1e92895be34062a67943777f4bc1670992702fb
David Dupplaw <david@dupplaw.me.uk>
2018-09-16 09:37:20 +0100

    refactor: DD: Tidy code a bit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -30,16 +30,6 @@ export class AppComponent implements OnInit {
   constructor() {
   }
 
-  static getParam(name: string): string {
-    const results = new RegExp('[\\?&]' as string + name + '=([^&#]*)').exec(window.location.href);
-
-    if (!results) {
-      return null;
-    }
-
-    return results[1] || null;
-  }
-
   static configValid() {
     return true; // this.gitlab && this.token && this.param_projects;
   }
@@ -77,32 +67,8 @@ export class AppComponent implements OnInit {
     const url = '/gitlab/projects';
     this.axios.get(url)
       .then((projectsResponse: AxiosResponse<Project[]>) => {
-
-        // Update the global commit summary
-        projectsResponse.data.forEach(project =>
-          this.commitSummary = this.commitSummary.add(project.commitSummary)
-        );
-
-        this.projects = projectsResponse.data.sort((a, b) => {
-          if ( !a.lastBuild && !b.lastBuild ) {
-            return a.name.localeCompare(b.name);
-          }
-          if ( !a.lastBuild ) {
-            return 1;
-          }
-          if ( !b.lastBuild ) {
-            return -1;
-          }
-
-          if (a.lastBuild.status === Status.PASS && b.lastBuild.status === Status.FAIL) {
-            return 1;
-          } else if (a.lastBuild.status === Status.FAIL && b.lastBuild.status === Status.PASS) {
-            return -1;
-          } else {
-            return a.name.localeCompare(b.name);
-          }
-        });
-
+        this.updateCommitSummary(projectsResponse);
+        this.projects = this.getSortedProjects(projectsResponse);
         this.changePageTitleAndFavIcon(projectsResponse);
 
         this.loading = false;
@@ -111,6 +77,34 @@ export class AppComponent implements OnInit {
         this.loading = false;
         this.errorMessage = `Error retrieving projects. Check your token and your GitLab host configuration. ${error}`;
       });
+  }
+
+  private updateCommitSummary(projectsResponse: AxiosResponse<Project[]>) {
+    projectsResponse.data.forEach(project =>
+      this.commitSummary = this.commitSummary.add(project.commitSummary)
+    );
+  }
+
+  private getSortedProjects(projectsResponse: AxiosResponse<Project[]>): Project[] {
+    return projectsResponse.data.sort((a, b) => {
+      if (!a.lastBuild && !b.lastBuild) {
+        return a.name.localeCompare(b.name);
+      }
+      if (!a.lastBuild) {
+        return 1;
+      }
+      if (!b.lastBuild) {
+        return -1;
+      }
+
+      if (a.lastBuild.status === Status.PASS && b.lastBuild.status === Status.FAIL) {
+        return 1;
+      } else if (a.lastBuild.status === Status.FAIL && b.lastBuild.status === Status.PASS) {
+        return -1;
+      } else {
+        return a.name.localeCompare(b.name);
+      }
+    });
   }
 
   getProjects() {
@@ -129,40 +123,13 @@ export class AppComponent implements OnInit {
         document.title = showingFailure ? this.okTitle : this.failTitle;
         this.favicon.href = showingFailure ? '/favicon.ico' : '/favicon-alert.ico';
       }, 1000);
+
     } else if (this.titleTimer) {
+
       // No broken builds?  Set the title and favicon to normal
       clearInterval(this.titleTimer);
       document.title = this.okTitle;
       this.favicon.href = '/favicon.ico';
     }
   }
-
-  // private storeBuildInformation(pipelineResponse, project) {
-  //   if (pipelineResponse) {
-  //     const startedFromNow = moment(pipelineResponse.started_at).fromNow();
-  //
-  //     this.buildMap.set(project.id, {
-  //       project: project.name,
-  //       id: pipelineResponse.id,
-  //       status: pipelineResponse.status,
-  //       started_at: startedFromNow,
-  //       started: pipelineResponse.started_at,
-  //       author: pipelineResponse.user.name,
-  //       project_path: project.path_with_namespace,
-  //       branch: pipelineResponse.ref
-  //     });
-  //
-  //     this.builds = Array.from(this.buildMap.values()).sort((a, b) => {
-  //       if (a.status === 'success' && b.status === 'failed') {
-  //         return 1;
-  //       } else if (a.status === 'failed' && b.status === 'success') {
-  //         return -1;
-  //       } else {
-  //         return a.project.localeCompare(b.project);
-  //       }
-  //     });
-  //
-  //     return pipelineResponse;
-  //   }
-  // }
 }
