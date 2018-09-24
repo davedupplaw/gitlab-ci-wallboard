307bd8a4a3e922aed8621515667c36fe9f0742e7
Dave Dupplaw <david.dupplaw@siemens.com>
2018-09-24 08:46:28 +0100

    fix: DD: Refactor chart card to reuse chart object

--- frontend/src/app/chart-card/chart-card.component.ts
+++ frontend/src/app/chart-card/chart-card.component.ts
@@ -25,45 +25,48 @@ export class ChartCardComponent implements OnInit {
   }
 
   ngOnInit() {
+    const ctx = this.rootElement.nativeElement.querySelector('#chart-card').getContext('2d');
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Loading'], //newData.getDataLabels(),
+        datasets: [{
+          label: '# of Votes',
+          data: [0],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 2
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
 
     this._data.subscribe(newData => {
-      const ctx = this.rootElement.nativeElement.querySelector('#chart-card').getContext('2d');
-      this.chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: newData.getDataLabels(),
-          datasets: [{
-            label: '# of Votes',
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 2,
-            data: newData.counts()
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-    });
+      this.chart.data.labels = newData.getDataLabels();
+      this.chart.data.datasets[0].data = newData.counts();
+      this.chart.update();
+    } );
   }
 }
